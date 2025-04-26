011fa0277e7832f86b5c0660874329cacd46c80e
VortexTeam <progaming1905kral@gmail.com>
2025-04-26 11:41:46 +0300

    VortexSpeed commit

---
+++ aw9BlfAtjgXS.net/javascript/ads/SHTyxAikNDwg.js
@@ -0,0 +1,6 @@
+/*
+  This script sets isBlocked to false if it runs.
+  If it's blocked (Adblockers should always block the script because the name is ads/ad.js),
+  then isBlocked stays true.
+*/
+window.isBlocked = false;
